e0a85ee46399d47abce82422fbba147aaf754954
SiwonChoi25 <chlznf@naver.com>
2025-08-08 16:38:16 +0900

    feat: db 세팅

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,7 +9,12 @@ export default defineConfig({
     proxy: {
       '/api': 'http://localhost:4000',
     },
-  }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
 
 
